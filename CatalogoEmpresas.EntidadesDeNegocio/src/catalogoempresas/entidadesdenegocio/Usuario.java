18e588955c9cd3c90a6fef0e0c4248f4b359c895
Nelson Carias <Carias030@outlook.com>
2023-08-08 17:20:14 -0600

    Programacion de las clases Empresa y Usuario

--- CatalogoEmpresas.EntidadesDeNegocio/src/catalogoempresas/entidadesdenegocio/Usuario.java
+++ CatalogoEmpresas.EntidadesDeNegocio/src/catalogoempresas/entidadesdenegocio/Usuario.java
@@ -1,5 +1,17 @@
 package catalogoempresas.entidadesdenegocio;
 
+import java.time.LocalDate;
+
 public class Usuario {
-    
+    private int id;
+    private int idRol;
+    private String nombre;
+    private String apellido;
+    private String login;
+    private String password;
+    private byte estatus;
+    private LocalDate fechaRegistro;
+    private int top_aux;
+    private String confirmPassword_aux;
+    private Rol rol;
 }
